06440c91ae0554fd502ee5ec3f9c7e0c2130f0b9
sfilipt <filip.strand@hotmail.com>
2023-12-13 15:27:21 +0100

    Fortsättning på Viktklassen
    SI till Amerikanska enheter fungerar

--- Vikt.cs
+++ Vikt.cs
@@ -6,80 +6,136 @@ using System.Threading.Tasks;
 
 namespace Projektuppgift
 {
-    internal class Vikt
+    public class Vikt
     {
-        static void Menu1()
+        public void Menu1()
         {
             int menuChoice = 0;
             int menuChoice2 = 0;
             int menuChoice3 = 0;
+            double amountToConvert = 0;
             double answer = 0;
+            string unit = "test";
+            string outputUnit = default;
             while (menuChoice != 3)
+            {
                 Console.WriteLine("\n ---------------------------------\n" +
                              "Vilken enhet vill du konvertera från?" +
                              "\n1. SI-enhet." +
                              "\n2. Amerikansk enheter." +
                              "\n3. Gå tillbaka till föregående meny.");
-            menuChoice = Convert.ToInt32(Console.ReadLine());
-            switch (menuChoice)
-            {
-                case 1:
-                    Menu2();
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-            }
-
-            void Menu2()
-            {
-                Console.WriteLine("\n -----------------------" +
-                            "\nVilken enhet vill du konvertera till?" +
-                            "\n1. SI-enhet." +
-                            "\n2. Amerikanka enhter" +
-                            "\n3. Gå tillbaka till föregående meny.");
-                menuChoice2 = Convert.ToInt32((Console.ReadLine()));
-                if (menuChoice2 == 1)
+                menuChoice = Convert.ToInt32(Console.ReadLine());
+                switch (menuChoice)
                 {
-                    Console.WriteLine("Vilket prefix vill du använda?" +
-                        "\n 1. Gram" +
-                        "\n 2. Kilogram" +
-                        "\n 3. Ton");
-                    menuChoice3 = Convert.ToInt32(Console.ReadLine());
+                    case 1:
+                        Console.WriteLine("Vilket prefix har storheten du vill omvandla från?" +
+                            "\n 1. Gram" +
+                            "\n 2. Kilogram" +
+                            "\n 3. Ton");
+                        menuChoice3 = Convert.ToInt32(Console.ReadLine());
+                        if (menuChoice3 == 1)
+                        {
+                            unit = "gram";
+                        }
+                        else if (menuChoice3 == 2)
+                        {
+                            unit = "kilogram";
+                        }
+                        else if (menuChoice3 == 3)
+                        {
+                            unit = "ton";
+                        }
+                        Menu2();
+                        break;
+                    case 2:
+                        Console.WriteLine("Vilket prefix har storheten du vill omvandla från?" +
+                           "\n 1. Ounce" +
+                           "\n 2. Pound" +
+                           "\n 3. Stone");
+                        menuChoice3 = Convert.ToInt32(Console.ReadLine());
+                        if (menuChoice3 == 1)
+                        {
+                            unit = "ounce";
+                        }
+                        else if (menuChoice3 == 2)
+                        {
+                            unit = "pound";
+                        }
+                        else if (menuChoice3 == 3)
+                        {
+                            unit = "stone";
+                        }
+                        break;
+                    case 3:
+                        break;
                 }
-                if (menuChoice2 == 2)
+
+                void Menu2()
                 {
-                    Console.WriteLine("Vilket prefix vill du använda?" +
-                        "\n 1. Ounce" +
-                        "\n 2. Pound" +
-                        "\n 3. Stone");
-                    menuChoice3 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("\n -----------------------" +
+                                "\nVilken enhet vill du konvertera till?" +
+                                "\n1. SI-enhet." +
+                                "\n2. Amerikanka enhter" +
+                                "\n3. Gå tillbaka till föregående meny.");
+                    menuChoice2 = Convert.ToInt32((Console.ReadLine()));
+                    switch (menuChoice2)
+                    {
+                        case 1:
+                            if (menuChoice == 1)
+                                SIToSI();
+                            if (menuChoice == 2)
+                                AmericanToSI();
+                            break;
+                        case 2:
+                            if (menuChoice == 1)
+                                SIToAmerican();
+                            Console.Write($"{amountToConvert} {unit} är {answer} {outputUnit}");
+                            if (menuChoice == 2)
+                                AmericanToAmerican();
+                            break;
+                        case 3:
+                            break;
+
+                    }
                 }
-                if ((menuChoice == 1) && (menuChoice2 == 1))
-                    SIToSI();
-                if ((menuChoice == 1) && (menuChoice2 == 2))
-                    SIToAmerican();
-                if ((menuChoice == 2) && (menuChoice2 == 2))
-                    AmericanToAmerican();
-                if ((menuChoice == 2) && (menuChoice2 == 2))
-                    AmericanToSI();
-            }
-            static void SIToAmerican()
-            {
+                double SIToAmerican()
+                {
+                    Console.WriteLine($"Hur många {unit} vill du omvandla?");
+                    amountToConvert = double.Parse(Console.ReadLine());
+                    if (unit == "gram")
+                    {
+                        outputUnit = "ounce";
+                        answer = amountToConvert * 0.035274;
+                    }
+                    else if (unit == "kilogram")
+                    { 
+                        outputUnit = "pound";
+                        answer = amountToConvert * 2.2046;
+                    }
+                    else if (unit == "ton")
 
-            }
-            static void AmericanToSI()
-            {
+                    {
+                        outputUnit = "stone";
+                        answer = amountToConvert * 157.473;
+                    }
+                    return answer;
 
-            }
-            static void AmericanToAmerican()
-            {
-
-            }
-            static void SIToSI()
-            {
-
+                }
+                void AmericanToSI()
+                {
+                    Console.WriteLine($"Hur många {0} vill du omvandla?", unit);
+                    Console.ReadLine();
+                }
+                void AmericanToAmerican()
+                {
+                    Console.WriteLine($"Hur många {0} vill du omvandla?", unit);
+                    Console.ReadLine();
+                }
+                void SIToSI()
+                {
+                    Console.WriteLine($"Hur många {0} vill du omvandla?", unit);
+                    Console.ReadLine();
+                }
             }
         }
     }
