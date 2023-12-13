ea2f15f628b4ac691f99bf449090bdc32f478f97
sfilipt <filip.strand@hotmail.com>
2023-12-13 16:06:44 +0100

    Lagt till metod för att bestämma prefix på output
    OutPutPrefix()

--- Vikt.cs
+++ Vikt.cs
@@ -12,7 +12,7 @@ namespace Projektuppgift
         {
             int menuChoice = 0;
             int menuChoice2 = 0;
-            int menuChoice3 = 0;
+            int inputPrefix = 0;
             double amountToConvert = 0;
             double answer = 0;
             string unit = "test";
@@ -23,48 +23,54 @@ namespace Projektuppgift
                              "Vilken enhet vill du konvertera från?" +
                              "\n1. SI-enhet." +
                              "\n2. Amerikansk enheter." +
-                             "\n3. Gå tillbaka till föregående meny.");
+                             "\n3. Gå tillbaka till föregående meny." +
+                             "\n -------------------------------");
                 menuChoice = Convert.ToInt32(Console.ReadLine());
                 switch (menuChoice)
                 {
                     case 1:
-                        Console.WriteLine("Vilket prefix har storheten du vill omvandla från?" +
+                        Console.WriteLine(" -------------------------------" +
+                            "\nVilket prefix har storheten du vill omvandla från?" +
                             "\n 1. Gram" +
                             "\n 2. Kilogram" +
-                            "\n 3. Ton");
-                        menuChoice3 = Convert.ToInt32(Console.ReadLine());
-                        if (menuChoice3 == 1)
+                            "\n 3. Ton" +
+                            "\n -------------------------------");
+                        inputPrefix = Convert.ToInt32(Console.ReadLine());
+                        if (inputPrefix == 1)
                         {
                             unit = "gram";
                         }
-                        else if (menuChoice3 == 2)
+                        else if (inputPrefix == 2)
                         {
                             unit = "kilogram";
                         }
-                        else if (menuChoice3 == 3)
+                        else if (inputPrefix == 3)
                         {
                             unit = "ton";
                         }
                         Menu2();
                         break;
                     case 2:
-                        Console.WriteLine("Vilket prefix har storheten du vill omvandla från?" +
+                        Console.WriteLine(" -------------------------------" +
+                            "\nVilket prefix har storheten du vill omvandla från?" +
                            "\n 1. Ounce" +
                            "\n 2. Pound" +
-                           "\n 3. Stone");
-                        menuChoice3 = Convert.ToInt32(Console.ReadLine());
-                        if (menuChoice3 == 1)
+                           "\n 3. Stone" +
+                           "\n -------------------------------");
+                        inputPrefix = Convert.ToInt32(Console.ReadLine());
+                        if (inputPrefix == 1)
                         {
                             unit = "ounce";
                         }
-                        else if (menuChoice3 == 2)
+                        else if (inputPrefix == 2)
                         {
                             unit = "pound";
                         }
-                        else if (menuChoice3 == 3)
+                        else if (inputPrefix == 3)
                         {
                             unit = "stone";
                         }
+                        Menu2();
                         break;
                     case 3:
                         break;
@@ -72,22 +78,25 @@ namespace Projektuppgift
 
                 void Menu2()
                 {
-                    Console.WriteLine("\n -----------------------" +
+                    Console.WriteLine("\n -------------------------------" +
                                 "\nVilken enhet vill du konvertera till?" +
                                 "\n1. SI-enhet." +
-                                "\n2. Amerikanka enhter" +
-                                "\n3. Gå tillbaka till föregående meny.");
+                                "\n2. Amerikanka enheter" +
+                                "\n3. Gå tillbaka till föregående meny." +
+                                "\n -------------------------------");
                     menuChoice2 = Convert.ToInt32((Console.ReadLine()));
                     switch (menuChoice2)
                     {
                         case 1:
                             if (menuChoice == 1)
+                                OutPutPrefix();
                                 SIToSI();
                             if (menuChoice == 2)
                                 AmericanToSI();
                             break;
                         case 2:
                             if (menuChoice == 1)
+                                OutPutPrefix();
                                 SIToAmerican();
                             Console.Write($"{amountToConvert} {unit} är {answer} {outputUnit}");
                             if (menuChoice == 2)
@@ -120,6 +129,52 @@ namespace Projektuppgift
                     }
                     return answer;
 
+                }
+                string OutPutPrefix()
+                {
+                    if (menuChoice2==1)
+                    {
+                        Console.WriteLine("------------------------------------" +
+                                   "\nVilket prefix ska resultatet av konverteringen ha?" +
+                                   "\n1. Gram" +
+                                   "\n2. Kilogram" +
+                                   "\n3. Ton");
+                        int outputPrefix = int.Parse(Console.ReadLine());
+                        if (outputPrefix == 1)
+                        {
+                            outputUnit = "gram";
+                        }
+                        else if(outputPrefix == 2) 
+                        {
+                            outputUnit = "kilogram";
+                        }
+                        else if(outputPrefix == 3)
+                        {
+                            outputUnit = "Ton";
+                        }
+                    }
+                    if (menuChoice2 == 2)
+                    {
+                        Console.WriteLine("------------------------------------" +
+                                   "\nVilket prefix ska resultatet av konverteringen ha?" +
+                                   "\n1. Ounce" +
+                                   "\n2. Pound" +
+                                   "\n3. Stone");
+                        int outputPrefix = int.Parse(Console.ReadLine());
+                        if (outputPrefix == 1)
+                        {
+                            outputUnit = "ounce";
+                        }
+                        else if (outputPrefix == 2)
+                        {
+                            outputUnit = "pound";
+                        }
+                        else if (outputPrefix == 3)
+                        {
+                            outputUnit = "stone";
+                        }
+                    }
+                    return outputUnit;
                 }
                 void AmericanToSI()
                 {
