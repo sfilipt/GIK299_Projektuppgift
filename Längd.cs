5bfc2ccb1dda4c3ebfc821548b8da031d1203bc1
h23maarv <h23maarv@du.se>
2023-12-13 17:38:49 +0100

    Uträkningar för cm,m,km och foot,yard,mile

--- Längd.cs
+++ Längd.cs
@@ -6,7 +6,136 @@ using System.Threading.Tasks;
 
 namespace Projektuppgift
 {
-    internal class Längd
+    public class Längd
     {
+        public void LengthConv();
+        {
+            Längd converter = new Längd();
+
+            while (true)
+            {
+
+                public double CentimeterToMeter(double Centimeter)
+                {
+                    return Centimeter / 100;
+                }
+                public double CentimeterToKilometer(double Centimeter)
+                {
+                    return Centimeter * 100000;
+                }
+                public double CentimeterToFoot(double Centimeter)
+                {
+                    return Centimeter / 2.54;
+                }
+                public double CentimeterToYards(double Centimeter)
+                {
+                    return Centimeter / 91.44;
+                }
+                public double CentimeterToMiles(double Centimeter)
+                {
+                    return Centimeter / 160934.4;
+                }
+                public double MeterToCentimeter(double Meters)
+                {
+                    return Meters * 100;
+                }
+                public double MeterToKilometer(double Meters)
+                {
+                    return Meters / 1000;
+                }
+                public double MeterToFoot(double Meters)
+                {
+                    return Meters / 0.3048;
+                }
+                public double MeterToYards(double Meters)
+                {
+                    return Meters / 0.9144;
+                }
+                public double MeterToMiles(double Meters)
+                {
+                    return Meters / 1609.344;
+                }
+                public double KilometerToCentimeter(double KiloMeters)
+                {
+                    return KiloMeters * 100000;
+                }
+                public double KilometerToMeter(double KiloMeters)
+                {
+                    return KiloMeters * 1000;
+                }
+                public double KilometerToFoot(double KiloMeters)
+                {
+                    return KiloMeters * 3280.84;
+                }
+                public double KilometerToYards(double KiloMeters)
+                {
+                    return KiloMeters / 0.0009144;
+                }
+                public double KilometerToMiles(double KiloMeters)
+                {
+                    return KiloMeters / 1.609344;
+                }
+                public double FootToCentimeter(double Foot)
+                {
+                    return Foot * 30.48;
+                }
+                public double FootToMeter(double Foot)
+                {
+                    return Foot * 0.3048;
+                }
+                public double FootToKilometer(double Foot)
+                {
+                    return Foot / 3280.84;
+                }
+                public double FootToYards(double Foot)
+                {
+                    return Foot / 3;
+                }
+                public double FootToMiles(double Foot)
+                {
+                    return Foot / 5280;
+                }
+                public double YardsToCentimeter(double Yard)
+                {
+                    return Yard * 91.44;
+                }
+                public double YardsToMeter(double Yard)
+                {
+                    return Yard * 0.9144;
+                }
+                public double YardsToKilometer(double Yard)
+                {
+                    return Yard * 0.0009144;
+                }
+                public double YardsToFoot(double Yard)
+                {
+                    return Yard * 3;
+                }
+                public double YardsToMiles(double Yard)
+                {
+                    return Yard / 1760;
+                }
+                public double MilesToCentimeter(double Miles)
+                {
+                    return Miles * 160934.4;
+                }
+                public double MilesToMeter(double Miles)
+                {
+                    return Miles * 1609.344;
+                }
+                public double MilesToKilometer(double Miles)
+                {
+                    return Miles * 1.60934;
+                }
+                public double MilesToFoot(double Miles)
+                {
+                    return Miles * 5280;
+                }
+                public double MilesToYards(double Miles)
+                {
+                    return Miles * 1760;
+                }
+            }
+        }
     }
 }
--- obj/Debug/net7.0/Projektuppgift.AssemblyInfo.cs
+++ obj/Debug/net7.0/Projektuppgift.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projektuppgift")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66ff4cdba9acabb32beebb7c422d83193ea69ab9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projektuppgift")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projektuppgift")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
