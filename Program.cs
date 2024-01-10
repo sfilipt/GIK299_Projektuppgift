480c15bd85d6cdf2f15174160980998f47c2a0af
h23maarv <h23maarv@du.se>
2024-01-10 16:36:45 +0100

    La till kommentarer

--- Program.cs
+++ Program.cs
@@ -4,10 +4,13 @@
     {
         static void Main(string[] args)
         {
+            // Variabler för användarens input och konverteringsresultat
             int menuLength = 0;
             string? menuInput = default;
+            // While-loop för att hålla programmet igång tills användaren väljer att avsluta
             while (menuLength != 4)
             {
+                // Menyutskrift
                 Console.WriteLine(
                     "\n-----Välkommen till Enhetskonverteringen-----" +
                     "\nVilken enhetskonvertering vill du testa?" +
@@ -18,8 +21,10 @@
                     "\n-----By: Filip Strand och Mattias Arvidsson-----\n"
                     );
 
+                // Läser användarens inmatning
                 menuInput = Console.ReadLine();
 
+                // Validerar inmatningen för att säkerställa att den är en siffra mellan 1 och 4
                 while (!int.TryParse(menuInput, out menuLength) || menuLength > 4 || menuLength < 1)
                 {
                     Console.WriteLine("\nFelaktig input, ange en siffra mellan 1 och 3.");
@@ -27,23 +32,29 @@
                     menuInput = Console.ReadLine();
                 }
 
+                // Använder en switch-sats för att välja rätt konverteringsmeny baserat på användarens val
                 switch (menuLength)
                 {
                     case 1:
+                        // Skapa en instans av Längd-klassen och anropa dess Menu1-metod
                         Längd längd = new Längd();
                         längd.Menu1();
                         break;
                     case 2:
+                        // Skapa en instans av Temperatur-klassen och anropa dess Menu1-metod
                         Temperatur temp = new Temperatur();
                         temp.Menu1();
                         break;
                     case 3:
+                        // Skapa en instans av Vikt-klassen och anropa dess Menu1-metod
                         Vikt vikt = new Vikt();
                         vikt.Menu1();
                         break;
                     case 4:
+                        // Avsluta programmet om användaren väljer alternativ 4
                         break;
                     default:
+                        // Meddelande om ogiltigt val
                         Console.WriteLine("Ogiltigt val. Försök igen. ");
                         break;
                 }
